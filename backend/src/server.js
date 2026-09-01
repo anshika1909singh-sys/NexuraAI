2aab9f080fd666eee91f0d713be5820eeeb3934d
Anshika Singh <anshika1909singh@gmail.com>
2026-09-01 11:49:19 +0530

    User Authentication initiated

---
+++ backend/src/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Nexura AI Backend is running",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Nexura backend running on port ${PORT}`);
+  });
+};
+
+startServer();
